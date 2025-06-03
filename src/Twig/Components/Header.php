36c5e8a2f7fb67e7902c3554cc575838f4d11c6f
Herman <nitrowitty@gmail.com>
2025-06-03 01:46:57 -0600

    cards

--- src/Twig/Components/Header.php
+++ src/Twig/Components/Header.php
@@ -7,4 +7,11 @@ use Symfony\UX\TwigComponent\Attribute\AsTwigComponent;
 #[AsTwigComponent]
 final class Header
 {
+	public string $image = "";
+
+	public string $title = '';
+
+	public string $description = '';
+
+	public array $featuredImages = [];
 }
