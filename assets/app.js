1a06452af97bd6c9e4ce7ca8b5c3c102b44c87d0
Herman <nitrowitty@gmail.com>
2025-06-02 23:24:55 -0600

    adding header and footer

--- assets/app.js
+++ assets/app.js
@@ -14,3 +14,35 @@ window.Alpine = Alpine
 Alpine.start()
 
 console.log('This log comes from assets/app.js - welcome to AssetMapper! 🎉');
+
+if (localStorage.getItem('dark-mode') === 'false' || !('dark-mode' in localStorage)) {
+    document.querySelector('html').classList.remove('dark');
+} else {
+    document.querySelector('html').classList.add('dark');
+}
+
+const lightSwitches = document.querySelectorAll('.light-switch');
+if (lightSwitches.length > 0) {
+    lightSwitches.forEach((lightSwitch, i) => {
+        if (localStorage.getItem('dark-mode') === 'true') {
+            // eslint-disable-next-line no-param-reassign
+            lightSwitch.checked = true;
+        }
+        lightSwitch.addEventListener('change', () => {
+            const { checked } = lightSwitch;
+            lightSwitches.forEach((el, n) => {
+                if (n !== i) {
+                    // eslint-disable-next-line no-param-reassign
+                    el.checked = checked;
+                }
+            });
+            if (lightSwitch.checked) {
+                document.documentElement.classList.add('dark');
+                localStorage.setItem('dark-mode', true);
+            } else {
+                document.documentElement.classList.remove('dark');
+                localStorage.setItem('dark-mode', false);
+            }
+        });
+    });
+}
